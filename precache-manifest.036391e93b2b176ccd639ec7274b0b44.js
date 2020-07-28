c54d1447aae034bb9c69c0a0f289b84b0ff573e9
shubham_panday <shubhamkumarpanday5@gmail.com>
2020-07-28 14:00:42 +0530

    Updates

---
+++ precache-manifest.036391e93b2b176ccd639ec7274b0b44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a969f4cb642d9bb65d82abe59955298",
+    "url": "/sample-project/index.html"
+  },
+  {
+    "revision": "de29ff55fce123f543ed",
+    "url": "/sample-project/static/css/main.bbd6a11a.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/sample-project/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sample-project/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de29ff55fce123f543ed",
+    "url": "/sample-project/static/js/main.ade63d67.chunk.js"
+  },
+  {
+    "revision": "5e1bdd808264f158d5ca",
+    "url": "/sample-project/static/js/runtime-main.b00b60bd.js"
+  }
+]);
